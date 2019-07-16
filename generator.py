77149c0837ff31a6c2a8837543397dbda602ef7f
Tierney Cyren <hello@bnb.im>
2019-07-16 18:15:11 -0400

    Add more entries, pulled from es-discuss

--- generator.py
+++ generator.py
@@ -1,5 +1,5 @@
 from textgenrnn import textgenrnn
 
 textgen = textgenrnn()
-textgen.train_from_file('titles.txt', num_epochs=1)
+textgen.train_from_file('titles-and-es-discuss-thread-titles.txt', num_epochs=1)
 textgen.generate()
